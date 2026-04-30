52de76b02736ce7e8c0b79a14ecc6a84a48cd21f
bruce <bruce.stockwell@gmail.com>
2026-04-29 22:42:59 -0400

    failed at a koan

---
+++ koans/09_shift_left/solution.asm
@@ -0,0 +1,12 @@
+%include "syscalls.inc"
+
+global _start
+
+section .text
+_start:
+        mov rax, 1
+        shl rax, 3
+        mov rdi, rax
+        mov rax, SYS_EXIT
+        syscall
+        ret
